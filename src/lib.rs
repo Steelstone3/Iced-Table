2df8f3e6898877b6d0a30990070dc19440e0d6cf
Skippy <steelstone3@gmail.com>
2025-10-16 02:22:11 +0100

    version-0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -3,37 +3,36 @@ use iced::{
     widget::{Column, Container, Row, container::Style, text},
 };
 
-const TEXT_SIZE: u16 = 16;
-const PADDING: u16 = 2;
-
 #[derive(Default, Debug, Clone)]
 pub struct Table {
     pub rows: Vec<Vec<String>>,
 }
 
 impl Table {
-    pub fn new(headers: Vec<String>, rows: Vec<Vec<String>>) -> Self {
+    pub fn new(headers: Vec<&str>, rows: Vec<Vec<&str>>) -> Self {
         let mut new_rows: Vec<Vec<String>> = vec![];
-        new_rows.push(headers);
+        new_rows.push(owned_rows(headers));
         for row in rows {
-            new_rows.push(row);
+            new_rows.push(owned_rows(row));
         }
 
         Self { rows: new_rows }
     }
 
-    pub fn add_headers(&mut self, headers: Vec<String>) {
-        self.rows.insert(0, headers);
+    pub fn add_headers(&mut self, headers: Vec<&str>) {
+        self.rows.insert(0, owned_rows(headers));
     }
 
-    pub fn add_row(&mut self, row: Vec<String>) {
-        self.rows.push(row);
+    pub fn add_row(&mut self, rows: Vec<&str>) {
+        self.rows.push(owned_rows(rows));
     }
 
     pub fn build<Message>(
         table_data: Table,
         color: Color,
         width: Option<f32>,
+        text_size: Option<u16>,
+        padding: Option<u16>,
     ) -> Element<'static, Message>
     where
         Message: Clone + Send + Sync + 'static,
@@ -42,7 +41,7 @@ impl Table {
 
         // with header and rows
         for row in table_data.rows {
-            let row = Table::with_row(row, color);
+            let row = Table::with_row(referenced_rows(&row), color, text_size, padding);
 
             table = table.push(row);
         }
@@ -56,15 +55,32 @@ impl Table {
         }
     }
 
-    fn with_row<Message>(rows: Vec<String>, color: Color) -> Row<'static, Message>
+    fn with_row<Message>(
+        rows: Vec<&str>,
+        color: Color,
+        text_size: Option<u16>,
+        padding: Option<u16>,
+    ) -> Row<'static, Message>
     where
         Message: Clone + Send + Sync + 'static,
     {
         let mut data_row = vec![];
+        let rows = owned_rows(rows);
+
+        let text_size = match text_size {
+            Some(text_size) => text_size,
+            None => 16,
+        };
+
+        let padding = match padding {
+            Some(padding) => padding,
+            None => 2,
+        };
+
         for row in rows.into_iter() {
             data_row.push(
-                Container::<Message, Theme, Renderer>::new(text(row).size(TEXT_SIZE))
-                    .padding(PADDING)
+                Container::<Message, Theme, Renderer>::new(text(row).size(text_size))
+                    .padding(padding)
                     .width(Length::Fill)
                     .height(Length::Shrink)
                     .max_height(22)
@@ -89,4 +105,14 @@ fn table_theme(color: Color) -> Style {
         },
         shadow: Default::default(),
     }
+}
+
+fn owned_rows(rows: Vec<&str>) -> Vec<String> {
+    let row_strings: Vec<String> = rows.iter().map(|s| s.to_string()).collect();
+
+    row_strings
+}
+
+fn referenced_rows(rows: &Vec<String>) -> Vec<&str> {
+    rows.iter().map(|s| s.as_str()).collect()
 }
