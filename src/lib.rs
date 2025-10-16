8a377d586349329dea9f921c0b938b4e07fc7e7e
Skippy <steelstone3@gmail.com>
2025-10-16 02:58:20 +0100

    version-0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ pub struct Table {
 }
 
 impl Table {
+    /// Typically use Table::default() then build using add_headers() and add_row()
+    /// However can provide a list of headers and rows to create Table
     pub fn new(headers: Vec<&str>, rows: Vec<Vec<&str>>) -> Self {
         let mut new_rows: Vec<Vec<String>> = vec![];
         new_rows.push(owned_rows(headers));
@@ -19,17 +21,42 @@ impl Table {
         Self { rows: new_rows }
     }
 
+    /// Add headers to the table
     pub fn add_headers(&mut self, headers: Vec<&str>) {
         self.rows.insert(0, owned_rows(headers));
     }
 
+    /// Add data rows to the table
     pub fn add_row(&mut self, rows: Vec<&str>) {
         self.rows.push(owned_rows(rows));
     }
 
+    /// Constructs a visually styled table component from structured data.
+    ///
+    /// This function iterates over the provided `Table` data, applying optional
+    /// styling parameters like `color`, `text_size`, and `padding` to each row.
+    /// The resulting table is wrapped in a `Container`, allowing for flexible width
+    /// control using the `width` parameter.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `Message`: The application's message type, used for handling user interaction events.
+    ///
+    /// # Arguments
+    ///
+    /// * `table_data`: The source data for the table, expected to contain a collection of rows.
+    /// * `color`: An optional `Color` to apply primary styling to the table or its elements. None sets the color to black. Consider "self.theme().palette().text" as a reasonable default.
+    /// * `width`: An optional fixed width (`f32`) for the container. If `None`, the container
+    ///   will fill the available space (`Length::Fill`).
+    /// * `text_size`: An optional font size (`u16`) for the text within the table cells. If `None` this will be set to size 16
+    /// * `padding`: An optional padding amount (`u16`) to apply around individual cells or rows. If `None` this will be set to padding 2
+    ///
+    /// # Returns
+    ///
+    /// An `Element<'static, Message>` representing the complete, iced table component
     pub fn build<Message>(
         table_data: Table,
-        color: Color,
+        color: Option<Color>,
         width: Option<f32>,
         text_size: Option<u16>,
         padding: Option<u16>,
@@ -57,7 +84,7 @@ impl Table {
 
     fn with_row<Message>(
         rows: Vec<&str>,
-        color: Color,
+        color: Option<Color>,
         text_size: Option<u16>,
         padding: Option<u16>,
     ) -> Row<'static, Message>
@@ -66,6 +93,11 @@ impl Table {
     {
         let mut data_row = vec![];
         let rows = owned_rows(rows);
+
+        let color = match color {
+            Some(color) => color,
+            None => Color::from_rgb(0.0, 0.0, 0.0),
+        };
 
         let text_size = match text_size {
             Some(text_size) => text_size,
